b15e61f482054f4a23e3acc60c3f09f87f6fc705
Andrii <gukandrew@gmail.com>
2020-03-13 13:17:45 +0200

    Add webpack-node-externals to skip externals from bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
   entry: './index.js',
@@ -11,10 +12,13 @@ module.exports = {
     __filename: false,  // without this line __filename returns / or blank string
   },
 
+  externals: [nodeExternals()], // need this line to avoid error if you work with Express framework
+
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
   },
+
   module: {
     rules: [
       {
@@ -26,6 +30,7 @@ module.exports = {
       }
     ]
   },
+
   plugins: [
     new CopyPlugin([
       { from: 'src/static', to: 'static' }
